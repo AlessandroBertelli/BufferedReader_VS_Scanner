b15768edfa2e6978fd74cf2441dd4cfda010e476
BertelLi.Alessandro <BertelLi.Alessandro@BIBLIO-PC07.OSDBSESTO.local>
2020-01-27 13:01:39 +0100

    fix generateFile

--- esercizioConFile/src/esercizioconfile/generateFile.java
+++ esercizioConFile/src/esercizioconfile/generateFile.java
@@ -1,5 +1,40 @@
 package esercizioconfile;
 
+import java.io.*;
+import java.util.*;
+
 public class generateFile {
+    
+    File file;
+    
+    public generateFile() throws IOException {
+        file = new File("file.txt");
+        if (file.exists()) {
+            file.delete();
+        } else {
+            file.createNewFile();
+        }
+    }
+    
+    public void popolaFile() throws IOException {
+        
+        FileWriter fw = new FileWriter(file);
+        
+        for (int i = 0; i < 100000; i++) {
+            String str = "";
+            for (int j = 0; j < 5; j++) {
+                str = generaNumero() + " ";
+            }
+            fw.write(str);
+            fw.write(System.getProperty("line.separator"));
+            fw.flush();
+        }
+        
+    }
+    
+    private int generaNumero() {
+        // Utilizzando la classe Random genero un numero casuale tra 0 e 254
+        return new Random().nextInt(254);
+    }
     
 }
