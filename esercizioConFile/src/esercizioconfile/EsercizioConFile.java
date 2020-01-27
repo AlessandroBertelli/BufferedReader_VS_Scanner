0f9cb2d8f22d5698c2046c81de92a89e96c875b4
BertelLi.Alessandro <BertelLi.Alessandro@BIBLIO-PC07.OSDBSESTO.local>
2020-01-27 13:40:28 +0100

    terminate program

--- esercizioConFile/src/esercizioconfile/EsercizioConFile.java
+++ esercizioConFile/src/esercizioconfile/EsercizioConFile.java
@@ -5,12 +5,18 @@ import java.io.IOException;
 
 public class EsercizioConFile {
 
-    public static void main(String[] args) throws IOException {       
-        
+    public static void main(String[] args) throws IOException {
+
         generateFile gf = new generateFile();
         gf.popolaFile();
-        
-        
+
+        readBuffer rb = new readBuffer();
+        rb.leggiFile();
+        //rb.stampaArray();
+
+        readScanner rs = new readScanner();
+        rs.leggiFile();
+        //rs.stampaArray();
     }
 
 }
--- esercizioConFile/src/esercizioconfile/readScanner.java
+++ esercizioConFile/src/esercizioconfile/readScanner.java
@@ -1,14 +1,55 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package esercizioconfile;
 
-/**
- *
- * @author BertelLi.Alessandro
- */
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+
 public class readScanner {
+    
+    
+    FileReader fr;
+    Scanner sc;
+    int[] array = new int[500000];
+    int counter = 0;
+
+    public readScanner() {
+        
+        try {
+            fr = new FileReader("file.txt");
+            sc = new Scanner(fr);
+        } catch (FileNotFoundException e) {
+            System.out.println("File non trovato!");
+        }
+    }
+        public void leggiFile() throws IOException {
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < 100000; i++) {
+            String s;
+            
+            //leggo la riga
+            s = sc.nextLine();
+            
+            //creo array contenenti i singoli valori sottoforma di stringa
+            String[] numStr = s.split(" ");
+            
+            //converto ognuno dei cinque numeri memorizzati nella stringa in intero e li inserisco nell'array
+            for (int j = 0; j < 5; j++) {                
+                array[counter++] = Integer.parseInt(numStr[j]);
+            }
+            
+        }
+        System.out.println("Il tempo di esecuzione con Scanner è di: "+(System.currentTimeMillis()-startTime)+" ms");
+    }
+    
+    public void stampaArray() {
+        
+        for (int i = 0; i <array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+    
     
 }
